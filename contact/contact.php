626b4b6a1037f103d8f012e859326a4c60e4f841
Trieu11092005 <trieuhqps39434@gmail.com>
2024-11-23 18:24:14 +0700

    Update contact.php

--- contact/contact.php
+++ contact/contact.php
@@ -48,9 +48,6 @@ if(empty($cont)||empty($sub)||empty($email)||empty($name)){
             </div>
             <div class="col-lg-5 mb-5">
                 <div class="bg-light p-30 mb-30">
-                    <iframe style="width: 100%; height: 250px;"
-                    src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3001156.4288297426!2d-78.01371936852176!3d42.72876761954724!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x4ccc4bf0f123a5a9%3A0xddcfc6c1de189567!2sNew%20York%2C%20USA!5e0!3m2!1sen!2sbd!4v1603794290143!5m2!1sen!2sbd"
-                    frameborder="0" style="border:0;" allowfullscreen="" aria-hidden="false" tabindex="0"></iframe>
                 <iframe src="https://www.google.com/maps/embed?pb=!1m14!1m12!1m3!1d27126.703768157127!2d106.61146743728206!3d10.827546948227296!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!5e0!3m2!1svi!2s!4v1732358840973!5m2!1svi!2s" width="440" height="400" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
                 </div>
                 <div class="bg-light p-30 mb-3">
